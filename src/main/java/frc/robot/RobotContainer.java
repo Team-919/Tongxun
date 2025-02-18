9bd0883122fca72f3a3e711b64f37fffbe938340
Tongxun <johndoe@example.com>
2025-02-18 16:09:06 -0500

    yes

---
+++ src/main/java/frc/robot/RobotContainer.java
@@ -0,0 +1,18 @@
+package frc.robot;
+
+import edu.wpi.first.math.system.plant.DCMotor;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
+
+public class RobotContainer {
+    private final DCMotor motor = DCMotor.getNeo550(1);
+    public RobotContainer() {
+        configureBindings();
+    }
+
+    private void configureBindings() {}
+
+    public Command getAutonomousCommand() {
+        return Commands.print(message:"No autonomous command configured");
+    }
+}
